49b44507fcc926270d2e1a78e4e8ca0e2fc7962e
mhcockpit <aviationmhcockpit@gmail.com>
2025-05-31 18:13:28 +0530

    settings change

--- digital_portfolio/settings.py
+++ digital_portfolio/settings.py
@@ -74,10 +74,10 @@ AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
-AWS_QUERYSTRING_AUTH = False
+# AWS_QUERYSTRING_AUTH = False
 
 # To serve media files with public URLs without query params
-AWS_QUERYSTRING_AUTH = False
+# AWS_QUERYSTRING_AUTH = False
 
 # Media
 PUBLIC_MEDIA_LOCATION = 'employees'
